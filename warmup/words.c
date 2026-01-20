aeef04b6ff5a9c89129bf7972c5ba1efa7039b68
John-Yf <johnyf7300@gmail.com>
2026-01-20 10:45:07 -0500

    words passes

--- warmup/words.c
+++ warmup/words.c
@@ -1,8 +1,14 @@
 #include "common.h"
 
-int
-main()
+void words(int n, char **args){
+	/* Prints each of the command line arguments on a new line.*/
+	for(int i = 1; i < n; i++){
+		printf("%s\n", args[i]);
+	} 
+}
+
+int main(int argc, char **argv)
 {
-	TBD();
+	words(argc, argv);
 	return 0;
 }
