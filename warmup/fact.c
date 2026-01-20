d6e92dad06481b09234d6f299b2631c924f8b5da
John-Yf <johnyf7300@gmail.com>
2026-01-20 11:22:54 -0500

    fact.c unfinished

--- warmup/fact.c
+++ warmup/fact.c
@@ -1,8 +1,29 @@
 #include "common.h"
 
-int
-main()
+int fact(int argc, char** args){
+	/*Uses recursion to calculate and print the factorial of 
+	the positive integer value (e.g., 5, 10, 030) passed in as 
+	an argument to the program, or prints the line "Huh?" if no argument is passed in, 
+	or if the first argument passed is not a positive integer 
+	(e.g., 0, negative value, decimal value, or non-integer value). 
+	If the value passed in exceeds 12, it prints "Overflow".*/
+
+	if(argc == 1){
+		printf("Huh?\n");
+		return 0;
+	} else {
+		int n = atoi(argv[1]);
+		if(){
+
+		} else if(n == 0 || n < 0 || ){
+
+		}
+	}
+
+}
+
+int main(int argc, char **argv)
 {
-	TBD();
+	fact(argc, argv);
 	return 0;
 }
