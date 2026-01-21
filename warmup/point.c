c338a65dbe7d0000c76347415a1881b5cbb0374d
John-Yf <johnyf7300@gmail.com>
2026-01-21 01:59:48 -0500

    point.c passes

--- warmup/point.c
+++ warmup/point.c
@@ -2,22 +2,34 @@
 #include "common.h"
 #include "point.h"
 
-void
-point_translate(struct point *p, double x, double y)
+#include <math.h>
+
+void point_translate(struct point *p, double x, double y)
 {
-	TBD();
+	p->x += x;
+	p->y += y;
 }
 
-double
-point_distance(const struct point *p1, const struct point *p2)
+double point_distance(const struct point *p1, const struct point *p2)
 {
-	TBD();
-	return -1.0;
+	double dx = p1->x - p2->x;
+	double dy = p1->y - p2->y;
+	double dist = sqrt(dx * dx + dy * dy);
+	return dist;
 }
 
-int
-point_compare(const struct point *p1, const struct point *p2)
+int point_compare(const struct point *p1, const struct point *p2)
 {
-	TBD();
-	return 0;
+	struct point origin;
+	point_set(&origin, 0.0, 0.0);
+	
+	double dist1 = point_distance(p1, &origin);
+	double dist2 = point_distance(p2, &origin);
+
+	if (dist1 < dist2)
+		return -1;
+	else if (dist1 > dist2)
+		return 1;
+	else
+		return 0;
 }
